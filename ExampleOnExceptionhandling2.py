e1c478f8865fbcac092c4ab10162cf02d1acd037
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2022-04-25 12:32:32 +0530

    Exception handling examples

---
+++ ExampleOnExceptionhandling2.py
@@ -0,0 +1,21 @@
+try:
+    num1, num2 = eval(input("Enter two numbers, separated by a comma : "))
+    result = num1 / num2
+    print("Result is", result)
+ 
+except ZeroDivisionError:
+    print("dont enter zero as denominator!!")
+ 
+except SyntaxError:
+    print("Comma is missing. Enter numbers separated by comma like this 1, 2")
+ 
+except:
+    print("Wrong input")
+ 
+else:
+    print("No exceptions")
+ 
+finally:
+    print("This will execute no matter what")#cleanup code
+
+
